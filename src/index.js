d85fcb442442fbe946ee2058c75140e2cef8a5bf
BiaSLou <bianca.slou@gmail.com>
2022-04-20 14:10:06 -0300

    Estrutura Básica da aplicação GoodPay

---
+++ src/index.js
@@ -0,0 +1,29 @@
+
+var janelaLogin = new bootstrap.Modal(document.getElementById("loginNegado"));
+var janelaPromocao = new bootstrap.Modal(document.getElementById("promo1"));
+
+function rotaCadastro() {
+    window.location.href ='../registro.html';
+}
+function rotaRecuperar() {
+    window.location.href = '../recuperar.html';
+}
+function rotaLogin(){
+    let email = document.getElementById("email").value;
+    let senha = document.getElementById("senha").value;
+    if (email === "victoricoma@gmail.com" && senha === "gordinho123") {
+    window.location.href ='../index.html';  
+} else {
+    janelaLogin.show();
+  }
+} 
+function alertaPromo(){
+    let titulo = "Promoção 1 [Quebra da Banca]";
+    let corpo = "Super Legal <br> <img src ='./img/4.png' width'160' height ='160'>";
+   
+    janelaPromocao.show();
+
+    document.getElementById("titulo").innerHTML = titulo;
+    document.getElementById("corpo").innerHTML = corpo + lista;
+
+}
